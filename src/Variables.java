5ed49ab2844949d3845deea27bfb593c9473a2b8
mvga <ing.mvasquezg@gmail.com>
2021-02-22 11:40:45 -0600

    Creando programa de declaracion de variables y tambien haciendo hincapie en los comentarios. Empezar video 7

---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		//Declaracion de variables tipo byte
+		//byte edad;
+		
+		//Declaracion de variables tipo short
+		//short edad;
+		
+		//Declaracion de variables tipo int
+		//int edad;
+				
+		//Inicar variable
+		//edad=35;
+		
+		//Declaracion e iniciazion de variable en la misma linea
+		int edad=35;
+		
+		//edad=75;
+		
+		System.out.println(edad);
+		
+		edad=75;
+
+		System.out.println(edad);
+	
+	}//end main
+
+}//end Variables
