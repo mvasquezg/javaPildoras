9631df434356eca06fd3b1028cd612fbb18389d8
mvga <ing.mvasquezg@gmail.com>
2021-02-22 12:13:21 -0600

    Aprendiendo a usar operadores en Java. Empezar video 8

---
+++ src/Operadores.java
@@ -0,0 +1,21 @@
+
+public class Operadores {
+
+	public static void main(String[] args) {
+			
+		int a=5;
+		int b;
+		
+		b=7;
+		
+		int c=a+b;
+		
+		//Operador incremento
+		c++;
+		
+		System.out.println(c);
+		
+		
+	}//end main
+
+}//end Operadores
