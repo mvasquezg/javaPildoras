959f851e428f7dc53974457f85593fe97769f726
mvga <ing.mvasquezg@gmail.com>
2021-02-22 13:51:44 -0600

    Modificando el programa para ocupar el metodo round, exponente

--- src/CalculosConMath.java
+++ src/CalculosConMath.java
@@ -2,9 +2,27 @@
 public class CalculosConMath {
 
 	public static void main(String[] args) {
-		double raiz = Math.sqrt(9);
-
-		System.out.println(raiz);
+		
+		//Metodo raiz cuadradada
+		//double raiz = Math.sqrt(9);
+		
+		//float num1 = 5.85F;
+		//int resultado=Math.round(num1);
+		
+		//Metodo para redondear round
+		/*double num1 = 9.55F;
+		int resultado=(int)Math.round(num1);
+		
+		System.out.println(resultado);*/
+		
+		//Metodo para calcular exponente
+		double base = 5;
+		double exponente = 3;
+		
+		int resultado=(int)Math.pow(base, exponente);
+		
+		System.out.println("El resultado de "+base+" elevado al "+exponente+" es "+resultado);
+		
 	}//end main
 
 }//end CalculosConMath
