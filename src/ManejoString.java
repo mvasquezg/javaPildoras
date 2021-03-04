023b8f8b1cc01afd0718767352015f3a61aca966
mvga <ing.mvasquezg@gmail.com>
2021-03-04 13:37:54 -0600

    Programa de manejo de cadenas de caracteres primeros metodos. Empezar video 12

---
+++ src/ManejoString.java
@@ -0,0 +1,28 @@
+
+public class ManejoString {
+
+	public static void main(String[] args) {
+		
+		String nombre="Miguel";
+		
+		System.out.println("Mi nombre es: "+nombre);
+		
+		System.out.println("Mi nombre tiene: "+nombre.length()+" letras");
+		
+		System.out.println("La primera letra de mi nombre es: "+nombre.charAt(0));
+		
+		int ultimaLetra;
+		
+		ultimaLetra=nombre.length();
+		
+		System.out.println("La ultima letra de mi nombre es: "+nombre.charAt(ultimaLetra-1));
+		
+		
+		
+				
+		
+		
+		
+	}//end main
+
+}//end ManejoString
