c46d169e18ce05f6f593d3b41d0801b16c62dbd2
mvga <ing.mvasquezg@gmail.com>
2021-03-04 16:42:12 -0600

    Usando la clase JOptionPane, para leer datos en pantalla

--- src/EntradaDatos.java
+++ src/EntradaDatos.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+//import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class EntradaDatos {
 
 	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
+		/*Scanner in=new Scanner(System.in);
 		String nombre;
 		int edad;
 		
@@ -13,8 +15,16 @@ public class EntradaDatos {
 		System.out.print("Ingresa tu Edad: ");
 		edad=in.nextInt();
 		
-		System.out.println("Hola "+nombre+" el anho que viene tendras: "+(edad+1)+" anhos");
+		System.out.println("Hola "+nombre+" el anho que viene tendras: "+(edad+1)+" anhos");*/
 		
+		//Video 15 Usando JOPTIONEPANE
+		String nombre;
+		int edad;
+		
+		nombre=JOptionPane.showInputDialog("Introduce tu nombre: ");
+		edad=Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad: "));
+		
+		System.out.println("Tu nombre es: "+nombre+" el anho que viene tendras: "+(edad+1));
 		
 	}//end main
 
