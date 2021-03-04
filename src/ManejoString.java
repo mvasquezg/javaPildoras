aba3393f738cebb8deab6248ae2c7e811257a6f9
mvga <ing.mvasquezg@gmail.com>
2021-03-04 15:02:41 -0600

    Se hace uso del metodo equals usando cadenas. Empezar video 13

--- src/ManejoString.java
+++ src/ManejoString.java
@@ -3,7 +3,7 @@ public class ManejoString {
 
 	public static void main(String[] args) {
 		
-		String nombre="Miguel";
+		/*String nombre="Miguel";
 		
 		System.out.println("Mi nombre es: "+nombre);
 		
@@ -16,6 +16,31 @@ public class ManejoString {
 		ultimaLetra=nombre.length();
 		
 		System.out.println("La ultima letra de mi nombre es: "+nombre.charAt(ultimaLetra-1));
+		
+		
+		//Codigo video 12 de pildoras informaticas manipulando cadenas
+		
+		//Metodo substring strae una cadena 
+		String frase="Hoy es un estupendo dia para programar en Java";
+		
+		//Primera psosiocn qe quiero extraer, segudan posicion la cual no qiiero extraer.
+		String fraseResumen=frase.substring(0,28) +  " irnos a la playa";
+		
+		System.out.println(fraseResumen);
+		*/
+		
+		//METODO eQUALS
+		String alumno1, alumno2;
+		
+		alumno1="David";
+		alumno2="david";
+		
+		//Equals evaual que si las dos cadenas soniguales consoiderando mayusvculas y misnucalas
+		//System.out.println(alumno1.equals(alumno2));
+		
+		//Equasl ignore case considera si las dos cadenas son iguales no importa si estan escritas en mayusuclas o minuculas
+		System.out.println(alumno1.equalsIgnoreCase(alumno2));
+		
 		
 		
 		
