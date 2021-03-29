cd31b97985cfc16c9553a59ccd2839e952af85c1
mvga <ing.mvasquezg@gmail.com>
2021-03-29 11:33:46 -0600

    Creando el primer programa haciendo uso del ciclo for, que imprime 10 veces el nombre

---
+++ src/UsoFor.java
@@ -0,0 +1,12 @@
+
+public class UsoFor {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i+".- Miguel");
+		}//end for 
+		
+	}//end main
+
+}//end UsoFor
