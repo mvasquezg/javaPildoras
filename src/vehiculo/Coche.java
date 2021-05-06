7d0cbc2932971376d07fe8b8b7bbf0d755c01790
mvga <ing.mvasquezg@gmail.com>
2021-05-05 21:43:48 -0500

    Se crea el primer programa orientado a objetos que es sobre un coche. Empezar video 29.

---
+++ src/vehiculo/Coche.java
@@ -0,0 +1,22 @@
+package vehiculo;
+
+public class Coche {
+   int ruedas;
+   int largo;
+   int ancho;
+   int motor;
+   int peso;
+   
+   //Metodo Constructor
+   public Coche() {
+	   ruedas=4;
+	   largo=2000;
+	   ancho=300;
+	   motor=1600;
+	   peso=500;
+	   
+   }//end Coche
+   
+   
+   
+}
---
+++ src/vehiculo/CocheApp.java
@@ -0,0 +1,15 @@
+package vehiculo;
+
+public class CocheApp {
+
+	public static void main(String[] args) {
+		Coche renault=new Coche();
+		
+		System.out.println("Este choche tiene: "+renault.ruedas+" ruedas.");
+		
+		
+		
+
+	}
+
+}
