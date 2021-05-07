b5131712d63f1c1d7098905412471c342722f0ab
mvga <ing.mvasquezg@gmail.com>
2021-05-06 23:15:16 -0500

    Se modifico la clase coche y la clase CocheApp, para que se pueda aplicar los metodos con paso de parametros. Empezar video 32

--- src/vehiculo/Coche.java
+++ src/vehiculo/Coche.java
@@ -21,11 +21,38 @@ public class Coche {
    }//end Coche
    
  //Metodos setter
-   public void setColor() {
-	   	color="Azul";
+   public void setColor(String color) {
+	   	//color="Azul";
+	   this.color=color;
    }//end setColor
    
+   public void setConfigurarAsientos(String asientoCuero) {
+	   
+	   if(asientoCuero=="Si") {
+		   this.asientoCuero=true;
+	   }else {
+		   this.asientoCuero=false;
+	   }
+	   
+   }//end setConfiguraAsientos
+   
+   
    //Metodos getter
+   public String getDatosGenerales() {
+	   return "La plataforma del vehiculo tiene "+ruedas+" ruedas. Mide "+(largo/1000)+" metros con un ancho: "+
+   ancho+" y un peso de plataforma de "+pesoPlataforma+" kg.";
+   }//end 
+   
+   public String getAsientos() {
+	   
+	   if(asientoCuero) {
+		   return "El coche tiene asientos de cuero"; 
+	   }else {
+		   return "El coche tiene asientos de Serie";
+	   }
+	   
+   }
+   
    public String getLargo() {
 	   return "El largo del coche es: "+largo+" cm\n";
    }//end getLargo
@@ -33,5 +60,7 @@ public class Coche {
    public String getColor() {
 	   return "El color del coche es: "+color+"\n";
    }
+   
+   
    
 }
--- src/vehiculo/CocheApp.java
+++ src/vehiculo/CocheApp.java
@@ -6,13 +6,18 @@ public class CocheApp {
 		Coche renault=new Coche();
 		
 		//System.out.println("Este choche tiene: "+renault.ruedas+" ruedas.");
-		renault.setColor();
+		renault.setColor("Amarillo");
 		
-		System.out.println(renault.getLargo()+renault.getColor());
+		//System.out.println(renault.getLargo()+renault.getColor());
 		
-		
+		System.out.println(renault.getDatosGenerales());
 		
 
-	}
+		System.out.println(renault.getColor());
+		
+		renault.setConfigurarAsientos("Si");
+		
+		System.out.println(renault.getAsientos());
+	}//end main
 
-}
+}//end CocheApp
