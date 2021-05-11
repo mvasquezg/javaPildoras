4a818a3c6da91a4ac99978e3c8efe1bc70a546d6
mvga <ing.mvasquezg@gmail.com>
2021-05-10 22:47:00 -0500

    Se cambio el paso de parametros de los metodos de cadena por un jOptionPane, empezar video 33

--- src/vehiculo/CocheApp.java
+++ src/vehiculo/CocheApp.java
@@ -1,12 +1,15 @@
 package vehiculo;
 
+import javax.swing.JOptionPane;
+
 public class CocheApp {
 
 	public static void main(String[] args) {
 		Coche renault=new Coche();
 		
 		//System.out.println("Este choche tiene: "+renault.ruedas+" ruedas.");
-		renault.setColor("Amarillo");
+		//renault.setColor("Amarillo");
+		renault.setColor(JOptionPane.showInputDialog("Ingrese el color del Coche"));
 		
 		//System.out.println(renault.getLargo()+renault.getColor());
 		
@@ -15,11 +18,13 @@ public class CocheApp {
 
 		System.out.println(renault.getColor());
 		
-		renault.setConfigurarAsientos("Si");
+		//renault.setConfigurarAsienTiene asientos de Cuero?tos("Si");
+		renault.setConfigurarAsientos(JOptionPane.showInputDialog("Tiene asientos de Cuero?"));
 		
 		System.out.println(renault.getAsientos());
 		
-		renault.setAireAcondicionado("Si");
+		//renault.setAireAcondicionado("Si");
+		renault.setAireAcondicionado(JOptionPane.showInputDialog("Tiene Aire acondicionado?"));
 		
 		System.out.println(renault.getAireAcondicionado());
 		
