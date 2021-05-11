392eea1a2c9011a225c36c16071d5dcc417a1ff0
mvga <ing.mvasquezg@gmail.com>
2021-05-10 22:48:39 -0500

    Se crean la clase empleado y la clase app, para crear objetos. se utiliza la clase date de java.util, empezar video 34.

---
+++ src/Empleado/Empleado.java
@@ -0,0 +1,21 @@
+package Empleado;
+
+import java.util.Date;
+
+public class Empleado {
+	
+	private String nombreEmpleado;
+	private double sueldoEmpleado;
+	private Date altaContrato; //Fecha de contrato
+	//private int anho;
+	//private int mes;
+	//private int dia;
+	
+	public Empleado(String nombreEmpleado, double sueldoEmpleado, int anho, int mes, int dia) {
+		
+		this.nombreEmpleado=nombreEmpleado;
+		this.sueldoEmpleado=sueldoEmpleado;
+		
+	}//end Empleado
+	
+}//Empleado
---
+++ src/Empleado/EmpleadoApp.java
@@ -0,0 +1,9 @@
+package Empleado;
+
+public class EmpleadoApp {
+
+	public static void main(String[] args) {
+
+	}//end main
+
+}//end EmpleadoApp
