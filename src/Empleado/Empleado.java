2ed62c58aafe2762606988d7202e3cc798c531d7
mvga <ing.mvasquezg@gmail.com>
2021-05-13 23:22:34 -0500

    Se modifica la clase empleado, se hace uso de la clase Gregorian Calendar y se crean los metodos para recuperar valores y asignar valores, Empezar video 35

--- src/Empleado/Empleado.java
+++ src/Empleado/Empleado.java
@@ -1,21 +1,55 @@
 package Empleado;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Empleado {
 	
 	private String nombreEmpleado;
 	private double sueldoEmpleado;
 	private Date altaContrato; //Fecha de contrato
-	//private int anho;
-	//private int mes;
-	//private int dia;
+	
 	
 	public Empleado(String nombreEmpleado, double sueldoEmpleado, int anho, int mes, int dia) {
 		
 		this.nombreEmpleado=nombreEmpleado;
 		this.sueldoEmpleado=sueldoEmpleado;
 		
+		GregorianCalendar calendario=new GregorianCalendar(anho, (mes-1), dia);
+		
+		this.altaContrato=calendario.getTime();
+		
 	}//end Empleado
+
+
+	public String getNombreEmpleado() {
+		return nombreEmpleado;
+	}
+
+
+	public void setNombreEmpleado(String nombreEmpleado) {
+		this.nombreEmpleado = nombreEmpleado;
+	}
+
+
+	public double getSueldoEmpleado() {
+		return sueldoEmpleado;
+	}
+
+
+	public void setSueldoEmpleado(double sueldoEmpleado) {
+		this.sueldoEmpleado = sueldoEmpleado;
+	}
+	
+	
+	public Date getAltaContrato() {
+		return altaContrato;
+	}
+	
+	public void setIncrementarSueldo(double porcentaje) {
+		double aumento=this.sueldoEmpleado*(porcentaje/100);
+		
+		this.sueldoEmpleado+=aumento;
+	}
 	
 }//Empleado
