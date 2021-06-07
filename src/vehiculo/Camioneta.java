5de9c3fdf2e3806ec2b8536dfe5f9bb9c22bc337
mvga <ing.mvasquezg@gmail.com>
2021-06-06 23:06:37 -0500

    Se creo la clase Camioneta, que es la subclase o clase hija de Coche. Se modifico la clase CocheApp, para imprimir los datos de la camioneta que hereda de coche. Empezar video 42

--- src/vehiculo/Camioneta.java
+++ src/vehiculo/Camioneta.java
@@ -12,4 +12,10 @@ public class Camioneta extends Coche{//hereda de la clase coche con la palabra e
 		this.extraCapacity=extraCapacity;
 		this.extraAsientos=extraAsientos;
 	}
+	
+	public String getDataFurgoneta() {
+		return "La capacidad de carga es: "+extraCapacity+" y los asientos adicionales son: "+extraAsientos+"\n";
+	}//end getDataFurgoneta
+	
+	
 }//end Camioneta
--- src/vehiculo/CocheApp.java
+++ src/vehiculo/CocheApp.java
@@ -1,11 +1,11 @@
 package vehiculo;
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 public class CocheApp {
 
 	public static void main(String[] args) {
-		Coche renault=new Coche();
+	/*	Coche renault=new Coche();
 		
 		//System.out.println("Este choche tiene: "+renault.ruedas+" ruedas.");
 		//renault.setColor("Amarillo");
@@ -32,7 +32,26 @@ public class CocheApp {
 		System.out.println(renault.getPesoTotalCoche());
 		
 		System.out.println("El precio del carro es: "+renault.getPrecioCoche());
+		*/
 		
+		
+		//Instancias muevas con herencia
+		Coche auto01=new Coche();
+		auto01.setColor("Amarillo");
+		
+		//Camioneta aplicanco la herencia
+		Camioneta camioneta01=new Camioneta(580, 7);
+		camioneta01.setColor("Rojo");
+		camioneta01.setConfigurarAsientos("Si");
+		camioneta01.setAireAcondicionado("Si");
+		
+		System.out.println("Coche: \n"+auto01.getDatosGenerales()+" "+auto01.getColor());
+		System.out.println("Camioneta: \n"+camioneta01.getDatosGenerales()+"\n"+camioneta01.getDataFurgoneta());
+		
+		
+		
+		
+	
 	}//end main
 
 }//end CocheApp
